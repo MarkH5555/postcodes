e6d82e92d7ffa189de08450a15f13f0088028f21
Mark Harrison <markharrison@Marks-MacBook-Pro.local>
2018-03-10 15:54:38 +0000

    added simple mapping

--- js/postcode.js
+++ js/postcode.js
@@ -1,42 +1,67 @@
 
-
-
-
 let url = "https://api.postcodes.io/postcodes/";
+let f1 = $("#field1");
+let f2 = $("#field2");
+let f3 = $("#field3");
+let f4 = $("#field4");
+let f5 = $("#field5");
+
+let map;
+let myLatLng;
+let marker;
 
 
 clearFields();
 
 
-function clearFields(){
-	$("#field1").val(" ");
-	$("#field2").val(" ");
-	$("#field3").val(" ");
-}
 
+
+
+
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 55, lng: -5}, zoom: 5
+	});
+}
 
 function getData(val) {
 
 	$.get( url + val, function(res, status){
 
-		$("#field1").val(res.result.admin_district);
-		$("#field2").val(res.result.region);
-		$("#field3").val(res.result.parliamentary_constituency);	
+		f1.val(res.result.admin_district);
+		f2.val(res.result.region);
+		f3.val(res.result.parliamentary_constituency);
+		f4.val(res.result.latitude);	
+		f5.val(res.result.longitude);
+
+		myLatLng = {lat: res.result.latitude, lng: res.result.longitude};	
+
+		marker = new google.maps.Marker({ position: myLatLng, title:"Location" });
+
+		marker.setMap(map);
 
 	});
 }
-
 
 $(".postcode-button").click(function(){
 
 	let pval = $("#postcode-input").val();
 
 	clearFields();
-	$("#field1").val("Postcode not found");
+	f1.val("Postcode not found");
 
 	getData(pval);
 
 });
+
+function clearFields(){
+	f1.val(" ");
+	f2.val(" ");
+	f3.val(" ");
+	f4.val(" ");
+	f5.val(" ");
+}
 
 
 
