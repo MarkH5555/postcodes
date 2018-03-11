f7021b5d4859a5402662192514daefc864929b24
Mark Harrison <markharrison@Marks-MacBook-Pro.local>
2018-03-11 22:14:24 +0000

    using $.ajax with error: success:  instead of $.get

--- js/postcode.js
+++ js/postcode.js
@@ -1,5 +1,5 @@
 
-let url = "https://api.postcodes.io/postcodes/";
+let apiUrl = "https://api.postcodes.io/postcodes/";
 let f1 = $("#field1");
 let f2 = $("#field2");
 let f3 = $("#field3");
@@ -29,21 +29,51 @@ function initMap() {
 
 function getData(val) {
 
-	$.get( url + val, function(res, status){
+	$.ajax({ 
 
-		f1.val(res.result.admin_district);
-		f2.val(res.result.region);
-		f3.val(res.result.parliamentary_constituency);
-		f4.val(res.result.latitude);	
-		f5.val(res.result.longitude);
+		url: apiUrl + val,
+		method: "GET",
+		success: function(res) {
 
-		myLatLng = {lat: res.result.latitude, lng: res.result.longitude};	
+			console.log("xxx");
 
-		marker = new google.maps.Marker({ position: myLatLng, title: val });
+			f1.val(res.result.admin_district);
+			f2.val(res.result.region);
+			f3.val(res.result.parliamentary_constituency);
+			f4.val(res.result.latitude);	
+			f5.val(res.result.longitude);
 
-		marker.setMap(map);
+			myLatLng = {lat: res.result.latitude, lng: res.result.longitude};	
 
+			marker = new google.maps.Marker({ position: myLatLng, title: val });
+
+			marker.setMap(map);
+
+		},
+
+		error: function(res) {
+
+			console.log("not found");
+			f1.val("Postcode not found");
+
+		}
 	});
+
+	// $.get( apiUrl + val, function(res, status){
+
+	// 	f1.val(res.result.admin_district);
+	// 	f2.val(res.result.region);
+	// 	f3.val(res.result.parliamentary_constituency);
+	// 	f4.val(res.result.latitude);	
+	// 	f5.val(res.result.longitude);
+
+	// 	myLatLng = {lat: res.result.latitude, lng: res.result.longitude};	
+
+	// 	marker = new google.maps.Marker({ position: myLatLng, title: val });
+
+	// 	marker.setMap(map);
+
+	// });
 }
 
 $(".postcode-button").click(function(){
@@ -51,7 +81,7 @@ $(".postcode-button").click(function(){
 	let pval = $("#postcode-input").val();
 
 	clearFields();
-	f1.val("Postcode not found");
+	
 
 	getData(pval);
 
